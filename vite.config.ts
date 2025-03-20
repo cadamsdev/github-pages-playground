685c2cfce121484a6f11ec5dcee8e43225ff4336
cadamsdev <cadamsdev@outlook.com>
2025-03-20 17:35:44 -0400

    setup workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const BASE_URL = '/github-pages-playground';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/github-pages-playground',
+  base: process.env.RUNWAY ? `${BASE_URL}/` + process.env.RUNWAY : BASE_URL,
 });
